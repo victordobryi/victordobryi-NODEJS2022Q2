9fcb3fdc8bba1692088089a6fdde090b99a18554
bazla <dobr1nya@mail.ru>
2022-06-09 10:16:59 +0300

    feat: add getName, getGreetingPhrase methods

--- file-manager/index.js
+++ file-manager/index.js
@@ -1,6 +1,24 @@
+import { getGreetingPhrase } from './utils/getGreetingPhrase.js';
+import { getName } from './utils/getName.js';
+import ReadLine from 'readline';
+import { stdin, stdout } from 'process';
+
+const readline = ReadLine.createInterface({
+  input: stdin,
+  output: stdout,
+});
+
 const fileManager = () => {
-  const userName = process.argv.slice(2);
-  console.log(userName);
+  const userName = getName();
+  const greetingPhrase = getGreetingPhrase(userName);
+  readline.on('line', (line) => {
+    if (line === 'get') {
+      console.log('Ты нажал гет');
+    } else if (line === 'gat') {
+      console.log('Ты нажал гfт');
+    }
+  });
+  console.log(greetingPhrase);
 };
 
 fileManager();
